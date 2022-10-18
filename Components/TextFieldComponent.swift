da6ba7a62088aaa2f067b2b33b1e80b2d73db7cb
임영준 <youngjun@Leo-MacBook-Pro.local>
2022-10-18 18:37:38 +0900

    signup 화면구현

---
+++ Components/TextFieldComponent.swift
@@ -0,0 +1,20 @@
+//
+//  TextFieldComponent.swift
+//  LinkFolder
+//
+//  Created by 임영준 on 2022/10/18.
+//
+
+import UIKit
+
+class TextFieldComponent: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
