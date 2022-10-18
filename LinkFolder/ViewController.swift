2486894297f6fa4fad5655763f9e6b00b72fc34e
임영준 <youngjun@Leo-MacBook-Pro.local>
2022-10-18 12:56:11 +0900

    링크폴더 프로젝트 시작

---
+++ LinkFolder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LinkFolder
+//
+//  Created by 임영준 on 2022/10/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LinkFolderUITests/LinkFolderUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  LinkFolderUITestsLaunchTests.swift
+//  LinkFolderUITests
+//
+//  Created by 임영준 on 2022/10/18.
+//
+
+import XCTest
+
+final class LinkFolderUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
