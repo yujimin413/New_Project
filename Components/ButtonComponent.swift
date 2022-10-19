d04d16d26bb9e2846619ee6cc4e9168b0e328ee4
임영준 <youngjun@Leo-MacBook-Pro.local>
2022-10-19 15:07:23 +0900

    로그인 화면 구현

--- Components/ButtonComponent.swift
+++ Components/ButtonComponent.swift
@@ -18,35 +18,14 @@ class ButtonComponent: UIButton {
         }
     }
     
+    func unactivateButton(){
+        self.isEnabled = false
+        self.backgroundColor = UIColor(named: "gray4")
+    }
     
-//    @IBOutlet weak var ButtonComponent: UIButton!
-//
-//    override init(frame: CGRect) {
-//            super.init(frame: frame)
-//            customInit()
-//            //alternativeCustomInit()
-//        }
-//
-//        required init?(coder: NSCoder) {
-//            super.init(coder: coder)
-//            customInit()
-//            //alternativeCustomInit()
-//        }
-//
-//        //방법 1: loadNibNamed(_:owner:options:) 사용
-//        func customInit() {
-//            if let view = Bundle.main.loadNibNamed("CustomNavigationBar", owner: self, options: nil)?.first as? UIView {
-//                view.frame = self.bounds
-//                addSubview(view)
-//            }
-//        }
-//
-//        //방법 2: UINib 생성 후 instantiate
-//        func alternativeCustomInit() {
-//            if let view = UINib(nibName: "CustomNavigationBar", bundle: nil).instantiate(withOwner: self, options: nil).first as? UIView {
-//                view.frame = self.bounds
-//                addSubview(view)
-//            }
-//        }
+    func activateButton(){
+        self.isEnabled = true
+        self.backgroundColor = .black
+    }
 }
 
--- Components/TextFieldComponent.swift
+++ Components/TextFieldComponent.swift
@@ -7,14 +7,6 @@
 
 import UIKit
 
-class TextFieldComponent: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+class TextFieldComponent: UITextField {
 
 }
