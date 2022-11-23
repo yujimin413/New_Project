14e10e9cdae1461bb440c39db001437869730455
leolim <dudwnssss@naver.com>
2022-11-23 09:42:35 +0900

    프로필 수정화면 구성

---
+++ ProfileEdit/ProfileEditViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileEditViewController.swift
+//  LinkFolder
+//
+//  Created by 임영준 on 2022/11/23.
+//
+
+import UIKit
+
+class ProfileEditViewController: UIViewController {
+
+    @IBOutlet weak var nicknameTextFeild: TextFieldComponent!
+    @IBOutlet weak var nicknameLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
